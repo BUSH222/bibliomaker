2ecee4777f7665519d148d0bdc488d64befe7639
Ted <58144503+BUSH222@users.noreply.github.com>
2024-03-17 15:19:36 +0300

    rsl search complete

--- helper1.py
+++ helper1.py
@@ -1,25 +1,63 @@
 ## File for tasks 1-5
 
 import requests
+import aiohttp
+import asyncio
 import re
+import json
 from datetime import datetime
 from functools import wraps
+from itertools import chain
 
 
 class BibEntry:
-    def __init__(self, authors: str, title: str, source: str, physical_desc: str) -> None:
+    """###One single bibliographical entry
+    Contains:
+    * authors - a string of all author names
+    * title - the title of the document
+    * source - the publisher of the document
+    * physical_desc - the physical description of the document
+    * tome - the tome information of the document
+
+    Methods:
+    * peek() - quickly print the contents of the entry
+    * __str__ - convert the entire bibliographic entry to a string
+    """
+
+    def __init__(self, authors: str, title: str, source: str, physical_desc: str, tome: str) -> None:
         self.authors = authors
         self.title = title
         self.source = source
         self.physical_desc = physical_desc
+        self.tome = tome
 
     def peek(self):
         """Print the contents of the entry, testing function"""
-        print(f'{self.authors} {self.title} // {self.source} {self.physical_desc}')
+        print(f'{self.authors} {self.title} // {self.source} {self.physical_desc} ! Том: {self.tome}')
 
     def __str__(self):
         """Return the contents of the entry."""
-        return f'{self.authors} {self.title} // {self.source} {self.physical_desc}'
+        return f'{self.authors} {self.title} // {self.source} {self.physical_desc} ! Том: {self.tome}'
+
+
+class Logger:
+    """The class for logging informational messages, used for debugging."""
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    def __init__(self, verbosity: bool) -> None:
+        self.verbosity = verbosity
+
+    def log(self, text, color=HEADER):
+        if self.verbosity:
+            print(f'{color} {text} {self.ENDC}')
 
 
 def handler(func):
@@ -187,7 +225,62 @@ def wikisearch(person, locale='ru', verbosity=False) -> (list[None] | list | Non
     return [dob, dod, pob, pod, pobdesc, poddesc]
 
 
-def rslsearch(person, fields=[]):
+async def rslsearch(person, verbosity=False, parallel=True) -> (None | list[BibEntry]):
+    """
+    ###Search for a person on the russian state library website.
+    ## Args:
+        * `person (str)` - name of the person to look up
+        * `verbosity (bool, default=False)` - [OPTIONAL] print additional information
+        * `parallel (bool, default=True)` - [OPTIONAL] speed up the search by using asynchronous requests
+    ## Returns:
+        * `None` - this person does not exist in the rsl
+        * `list[BibEntry]` - list of bibliographical entries found in the rsl
+    """
+
+    async def fetch_pages(session, url, data, num):
+        data['SearchFilterForm[page]'] = num + 1
+        async with session.get(url, data=data, timeout=10) as response:
+            r_l = await response.text()
+            return re.findall(PATTERN, json.loads(r_l)['content'])
+
+    async def fetch_entry(session, url):
+        URL2 = 'https://search.rsl.ru'
+        async with session.get(URL2+url, timeout=10) as response:
+            hit = await response.text()
+            author = ' '.join(re.findall(r'<td itemprop="author">(.*?)<\/td>', hit))
+            title = ' '.join(re.findall(r'<td itemprop="name">(.*?)<\/td>', hit))
+            publisher = ' '.join(re.findall(r'<th>Выходные данные<\/th><td>(.*?)<\/td>', hit))
+            physical_desc = ' '.join(re.findall(r'<th>Физическое описание<\/th><td>(.*?)<\/td>', hit))
+            tome = ' '.join(re.findall(r'<th>Том<\/th><td>(.*?)<\/td>', hit))
+            return BibEntry(author, title, publisher, physical_desc, tome)
+
+    def non_parallel_rslsearch(logger, URL, URL2, PATTERN, reqdata):
+        logger.log('Non-parallel search specified, starting...')
+        entries = []
+        r = requests.get(URL, data=reqdata).json()
+        maxpage = r['MaxPage']
+        totalhits = r['TotalHits']
+        logger.log(f'Found, number of pages: {maxpage}, number of hits {totalhits}; Fetching pages')
+
+        hits = re.findall(PATTERN, r['content'])
+        for i in range(1, r['MaxPage']):
+            reqdata['SearchFilterForm[page]'] = i + 1
+            r_l = requests.get(URL, data=reqdata).json()
+            hits.extend(re.findall(PATTERN, r_l['content']))
+
+        logger.log('Gathering bibliographical info...')
+        for p in hits:
+            hit = requests.get(URL2+p).text
+            author = ' '.join(re.findall(r'<td itemprop="author">(.*?)<\/td>', hit))
+            title = ' '.join(re.findall(r'<td itemprop="name">(.*?)<\/td>', hit))
+            publisher = ' '.join(re.findall(r'<th>Выходные данные<\/th><td>(.*?)<\/td>', hit))
+            physical_desc = ' '.join(re.findall(r'<th>Физическое описание<\/th><td>(.*?)<\/td>', hit))
+            tome = ' '.join(re.findall(r'<th>Том<\/th><td>(.*?)<\/td>', hit))
+            entries.append(BibEntry(author, title, publisher, physical_desc, tome))
+        logger.log('Done!')
+        return entries
+
+    logger = Logger(verbosity=verbosity)
     URL = 'https://search.rsl.ru/site/ajax-search?language=ru'
     URL2 = 'https://search.rsl.ru'
     PATTERN = r'href=\"(\/ru\/record\/\d*?)\"'
@@ -197,26 +290,38 @@ def rslsearch(person, fields=[]):
         "SearchFilterForm[search]": f"author:(\"{person.replace(' ', '+')}\")",
         "SearchFilterForm[fulltext]": "0",
         "SearchFilterForm[updatedFields][]": "search"}
-    
+
     entries = []
+
+    logger.log('Starting the RSL search...')
+
+    if not parallel:
+        return non_parallel_rslsearch(logger, URL, URL2, PATTERN, reqdata)
 
     r = requests.get(URL, data=reqdata).json()
     hits = re.findall(PATTERN, r['content'])
-    for i in range(1, r['MaxPage']):
-        reqdata['SearchFilterForm[page]'] = i + 1
-        r_l = requests.get(URL, data=reqdata).json()
-        hits.extend(re.findall(PATTERN, r_l['content']))
-    print(len(hits))
-    print(r['TotalHits'])
+    maxpage = r['MaxPage']
+    totalhits = r['TotalHits']
 
-    for p in hits:
-        hit = requests.get(URL2+p).text
-        author = ' '.join(re.findall(r'<td itemprop="author">(.*?)<\/td>', hit))
-        title = ' '.join(re.findall(r'<td itemprop="name">(.*?)<\/td>', hit))
-        publisher = ' '.join(re.findall(r'<th>Выходные данные<\/th><td>(.*?)<\/td>', hit))
-        physical_desc = ' '.join(re.findall(r'<th>Физическое описание<\/th><td>(.*?)<\/td>', hit))
-        entries.append(BibEntry(author, title, publisher, physical_desc))
+    logger.log(f'Found, number of pages: {maxpage}, number of hits {totalhits}; Fetching pages')
 
+    async with aiohttp.ClientSession() as session1:
+        tasks1 = [fetch_pages(session1, URL, reqdata, i) for i in range(1, maxpage)]
+        results1 = await asyncio.gather(*tasks1)
+        results1 = list(set(chain(*results1)))
+        hits.extend(results1)
+
+    logger.log(f'Found {len(hits)} pages')
+    if len(hits) > 50:
+        logger.log('Sleeping to prevent rate limits')
+        await asyncio.sleep(5)
+    logger.log('Gathering bibliographical info...')
+    async with aiohttp.ClientSession() as session2:
+        tasks2 = [fetch_entry(session2, p) for p in hits]
+        results2 = await asyncio.gather(*tasks2)
+
+        entries.extend(results2)
+    logger.log('Done!')
     return entries
 
 
@@ -226,8 +331,9 @@ def tester():
     # persontest2 = 'Обручев Владимир Афанасьевич'
     # persontest3 = 'Сумгин Михаил Иванович'
     # persontest4 = 'Вознесенский Владимир Александрович'
-    res = rslsearch(persontest1)
-    print(str(res[-1]))
+    res = asyncio.run(rslsearch(persontest1, verbosity=True, parallel=True))
+    # print('\n'.join(list(map(str, r  es))))
+    print(len(res))
 
 
 tester()
