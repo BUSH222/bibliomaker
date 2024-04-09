1ed4fca7c4eb1519d01848ad3a8107c814f62a92
Ted <58144503+BUSH222@users.noreply.github.com>
2024-04-09 09:15:18 +0300

    minor bugfix

--- helper2.py
+++ helper2.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from helper1 import Logger
+from helper1 import Logger, handler
 
 
-def rgo_check(name):
+def rgo_check(name, verbosity=True):
+    logger = Logger(verbosity=verbosity)
+    logger.log('Checking if a person exists in rgo...')
     URL = f"https://elib.rgo.ru/simple-search?location=%2F&query={name}&rpp=10&sort_by=score&order=desc"
     htm = requests.get(URL).text
     soup = BeautifulSoup(htm, "html.parser")
@@ -22,6 +24,8 @@ def rgo_check(name):
             'etal': '0',
             'start': '0'
         }
+        logger.log('Obtaining a description from the cards')
+
         for j in range(0, int(limit), 10):
             params['start'] = str(j)
             urlsec = requests.get("https://elib.rgo.ru/simple-search", params=params).text
@@ -34,17 +38,23 @@ def rgo_check(name):
                 urlres[cnt] = f"https://elib.rgo.ru/{str(urlres[cnt])[45:68]}"
                 res[textres[cnt]] = str(urlres[cnt])
                 cnt += 1
+        logger.log("Done")
         return res
     else:
-        return page_p.string
+        logger.log("Not found, exiting")
+        return None
 
 
-def rnb_check(name):
+@handler
+def rnb_check(name, verbosity=True):
+    logger = Logger(verbosity=verbosity)
+    logger.log('Checking if a person exists in rnb...')
     URL = f"https://nlr.ru/e-case3/sc2.php/web_gak/ss?text={name}&x=15&y=17"
     htm = requests.get(URL).text
     soup = BeautifulSoup(htm, "html.parser")
     allcards = soup.find("div", id="row1textmain").find("div", class_="text").find_all("a", href_="")
     out = {}
+    logger.log('Obtaining a description from the cards')
     for i in allcards:
         if name in str(i.string):
             # res[i.string] = "https//nlr.ru/e-case3/sc2.php/web_gak{}".format(str(i["href"])[2:])
@@ -61,10 +71,14 @@ def rnb_check(name):
                 heading = soupcrd.find("div", class_="center").find("b").string
                 pict = soupcrd.find("img", class_="card")["src"]
                 out[heading[:-1]] = f'https://nlr.ru{pict}'
+    logger.log('Done!')
     return out
 
 
-def nnr_check(name):
+@handler
+def nnr_check(name, verbosity=True):
+    logger = Logger(verbosity=verbosity)
+    logger.log('Checking if a person exists in nnr...')
     URL = "http://e-heritage.ru/Catalog/FindPerson"
     data = {
         'maxRow': '1',
@@ -83,7 +97,7 @@ def nnr_check(name):
     htm = requests.post(URL, data=data).text
     soup = BeautifulSoup(htm, "html.parser")
     urlres = soup.find_all("a")
-
+    logger.log('Obtaining a description from the cards')
     for j in urlres:
         suburl = str(j["href"])
         suburl = requests.get(f"http://e-heritage.ru{suburl}").text
@@ -110,11 +124,14 @@ def nnr_check(name):
 
         res[biores1[-1].string.replace("\r\n                ", "").replace("    ", "")] = \
             [i.string for i in biores2[-1].find_all("li")]
-
+        logger.log('Done!')
         return res, crdres
 
 
-def spb_check(name):
+@handler
+def spb_check(name, verbosity=True):
+    logger = Logger(verbosity=verbosity)
+    logger.log('Checking if a person exists in spb...')
     URL = 'https://primo.nlr.ru/primo_library/libweb/action/search.do'
     params = {
         'fn': 'search',
@@ -125,6 +142,7 @@ def spb_check(name):
     crd = soup.find_all("h2", class_="EXLResultTitle")
     headres = []
     bodyres = []
+    logger.log('Obtaining a description from the cards')
     for i in crd:
         surl = requests.get("https://primo.nlr.ru/primo_library/libweb/action/{}".format(i.find("a")["href"])).text
         sou = BeautifulSoup(surl, "html.parser")
@@ -137,8 +155,10 @@ def spb_check(name):
                 find("span", class_="EXLDetailsDisplayVal")
         headres.append(sou.find("h1", class_="EXLResultTitle").string)
         bodyres.append(descrip.string)
+    logger.log('Done!')
     return headres, bodyres
 
 
 if __name__ == "__main__":
-    print('\n'.join([f'{key}:   {value}' for key, value in rnb_check('Обручев Владимир Афанасьевич').items()]))
+    # print('\n'.join([f'{key}:   {value}' for key, value in rnb_check('Обручев Владимир Афанасьевич').items()]))
+    print(rgo_check("Русаков Владимир Афанасьевич"))
