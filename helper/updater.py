ed7495acdb3dcaffbc3583bb64e5dca018beb857
nexxy06 <nikpan254@gmail.com>
2024-07-07 16:58:44 +0300

    updater

--- helper/updater.py
+++ helper/updater.py
@@ -1,2 +1,11 @@
 import urllib.request
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) + r"\bibliomaker.exe"
+
+if os.path.isfile(dir_path):
+    os.remove(dir_path)
+else:
+    print("Error: %s file not found" % dir_path)
+
 urllib.request.urlretrieve("https://github.com//BUSH222/bibliomaker/releases/download/v1.2/bibliomaker.exe", "bibliomaker.exe")
